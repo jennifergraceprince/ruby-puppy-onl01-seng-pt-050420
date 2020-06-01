1ed86e28be144671ec7b57c309757cb82575dafd
Jennifer Prince <jennifergraceprince@gmail.com>
2020-06-01 04:50:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,26 @@
 class Dog
   attr_accessor :name
-
   @@all = []
-
   def initialize(name)
     @name = name
-    @@all << self
+    save
+  end
+  def name
+    @name
+  end
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each {|pooch| puts pooch.name}
   end
 
   def self.clear_all
     @@all.clear
   end
-  
-  def self.print_all
-    @@all.each do |d|
-      print d.name
-    end
-  end
-  
-  def self.all
-    @@all.each do |d|
-      puts d.name
-    end
+
+  def save
+    @@all << self
   end
 end
